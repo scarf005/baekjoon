672a415eb89991107255f331fd12568648eaccf1
scarf <greenscarf005@gmail.com>
2022-12-31 23:18:41 +0900

    feat: save test input to yaml

--- script/template.ts
+++ script/template.ts
@@ -1,42 +1,27 @@
+import { stringify } from 'https://deno.land/std@0.170.0/encoding/yaml.ts'
 import { fmti } from 'https://raw.githubusercontent.com/scarf005/fmti/main/main.ts'
 import { Meta, Sample } from './types.ts'
 
 const idNameGen = (id: number) => `\`${id}\``
 export const urlGen = (id: number) => `https://www.acmicpc.net/problem/${id}`
 
-export const testText = (id: number, samples: Sample[]) => {
-  const idName = idNameGen(id)
-
-  const testcase = (sample: Sample, i: number) => fmti`
-      "case ${i}" {
-          val input =
-              """
-              ${fmti`${sample.input}`}
-              """
-          val output =
-              """
-              ${fmti`${sample.output}`}
-              """
-          testCase(input, output, ::${idName})
-      }
-  `
-
-  return fmti`
+export const testObject = (id: number) =>
+  fmti`
       import io.kotest.core.spec.style.StringSpec
-      import utils.testCase
+      import utils.Examples
 
       object _${id}KtTest : StringSpec({
-          ${samples
-            .map((s, i) => testcase(s, i + 1))
-            .map(x => fmti`${x}`)
-            .join('\n')}
+          "example" {
+              Examples.test(::${idNameGen(id)})
+          }
       })
   `
-}
+
+export const testYaml = (samples: Sample[]) => stringify({ cases: samples })
 
 const section = (sx: readonly string[]) => sx.map(x => `* ${x}`).concat('*')
 
-export const problemText = (id: number, meta: Meta) => {
+export const problemKDoc = (id: number, meta: Meta) => {
   const title = section([`# ${meta.title}`])
   const desc = section([`## 문제`, ...meta.desc.split('\n')])
   const input = section([`## 입력`, ...meta.input.split('\n')])
