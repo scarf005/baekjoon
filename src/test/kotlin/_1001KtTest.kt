b4fbdd58dec4f50f2609e8b7f5799bd31e09ce9f
scarf <greenscarf005@gmail.com>
2022-12-31 23:25:53 +0900

    feat: example problems

---
+++ src/test/kotlin/_1001KtTest.kt
@@ -0,0 +1,8 @@
+import io.kotest.core.spec.style.StringSpec
+import utils.Examples
+
+object _1001KtTest : StringSpec({
+    "example" {
+        Examples.test(::`1001`)
+    }
+})
---
+++ src/test/kotlin/_10828KtTest.kt
@@ -0,0 +1,8 @@
+import io.kotest.core.spec.style.StringSpec
+import utils.Examples
+
+object _10828KtTest : StringSpec({
+    "example" {
+        Examples.test(::`10828`)
+    }
+})
