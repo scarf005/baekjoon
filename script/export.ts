45acf9d30624aeea4cd601fb995855665fe26dba
scarf <greenscarf005@gmail.com>
2023-01-01 23:32:38 +0900

    fix: template oddities (#12)
    * fix: tests not having final newline
    
    * fix: functions above main being ignored

--- script/export.ts
+++ script/export.ts
@@ -3,15 +3,14 @@ import clipboard from 'https://deno.land/x/clipboard@v0.0.2/mod.ts'
 import { open } from 'https://deno.land/x/open@v0.0.5/index.ts'
 
 const problems = await recursiveReaddir('src/main/kotlin')
+const see = /\/\*\* \[See\].*\n/
 
-const extract = (id: number, text: string) => {
-  const signature = `fun \`${id}\`()`
-  const imports = text.match(/import .+/g)?.join('\n') ?? ''
-  const start = text.indexOf(signature)
-  return [imports, text.substring(start).replace(signature, 'fun main()')]
-    .join('\n\n')
+const extract = (id: number, text: string) =>
+  text
+    .replace(/package .+/, '')
+    .replace(see, '')
+    .replace(`fun \`${id}\`()`, 'fun main()')
     .trim()
-}
 
 const post = async (id: number) => {
   const problem = problems.find(file => file.endsWith(`${id}.kt`))
