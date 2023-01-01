31f9c9d1c5dc82e7bc617c2f91290e328c1e4ce1
scarf <greenscarf005@gmail.com>
2023-01-01 17:28:39 +0900

    chore: remove test solutions

--- src/test/kotlin/_10828KtTest.kt
+++
@@ -1,8 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import utils.Examples
-
-object _10828KtTest : StringSpec({
-    "example" {
-        Examples.test(::`10828`)
-    }
-})
--- src/test/kotlin/_2558KtTest.kt
+++
@@ -1,8 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import utils.Examples
-
-object _2558KtTest : StringSpec({
-    "example" {
-        Examples.test(::`2558`)
-    }
-})
