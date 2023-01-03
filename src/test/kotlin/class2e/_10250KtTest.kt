807002dc1c7a084344bcd3601bb3925598432a4d
scarf <greenscarf005@gmail.com>
2023-01-03 17:48:39 +0900

    chore: generated tests for class 2

---
+++ src/test/kotlin/class2e/_10250KtTest.kt
@@ -0,0 +1,10 @@
+package class2e
+
+import io.kotest.core.spec.style.StringSpec
+import utils.Examples
+
+object _10250KtTest : StringSpec({
+    "example" {
+        Examples.test(::`10250`)
+    }
+})
