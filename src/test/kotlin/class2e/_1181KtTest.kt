4c293e4938960776032fee3135080f57627a67eb
scarf <greenscarf005@gmail.com>
2023-01-03 17:51:38 +0900

    chore: generated tests for class 2 (#18)

---
+++ src/test/kotlin/class2e/_1181KtTest.kt
@@ -0,0 +1,10 @@
+package class2e
+
+import io.kotest.core.spec.style.StringSpec
+import utils.Examples
+
+object _1181KtTest : StringSpec({
+    "example" {
+        Examples.test(::`1181`)
+    }
+})
---
+++ src/test/kotlin/class2e/_2751KtTest.kt
@@ -0,0 +1,10 @@
+package class2e
+
+import io.kotest.core.spec.style.StringSpec
+import utils.Examples
+
+object _2751KtTest : StringSpec({
+    "example" {
+        Examples.test(::`2751`)
+    }
+})
