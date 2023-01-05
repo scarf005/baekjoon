7ebc29ac5579703576eeab58b26f9acacc975262
scarf <greenscarf005@gmail.com>
2023-01-05 16:41:38 +0900

    refactor: cleanup test migration (#23)
    * test: ignore private constructor warning
    
    no point checking input and output again when copying
    
    * test: remove unused function
    
    feature migrated to Runner

--- src/test/kotlin/utils/Examples.kt
+++ src/test/kotlin/utils/Examples.kt
@@ -1,42 +1,15 @@
 package utils
 
 import com.charleskorn.kaml.Yaml
-import io.kotest.inspectors.forAll
-import io.kotest.matchers.doubles.plusOrMinus
-import io.kotest.matchers.shouldBe
 import kotlinx.serialization.Serializable
-import kotlin.reflect.KFunction
 
 @Serializable
 data class Examples(val samples: List<TestCase>) {
-    fun test(fn: () -> Unit) = samples.forAll { (input, output) ->
-        mock(input.trimIndent()) { fn() } shouldBe output.toOutput()
-    }
-
-    fun testNearEquality(fn: () -> Unit, tolerance: Double) = samples.forAll { (input, output) ->
-        mock(input.trimIndent()) { fn() }.let(String::toDouble)
-            .shouldBe(output.toOutput().toDouble() plusOrMinus tolerance)
-    }
-
     companion object {
         private fun resourceStream(id: Int) =
             Examples::class.java.getResourceAsStream("/$id.yaml")!!
 
         fun fromResource(id: Int): Examples =
             Yaml.default.decodeFromStream(serializer(), resourceStream(id))
-
-        fun test(fn: KFunction<*>) {
-            val name = fn.name
-            println("Testing $name")
-            fn.returnType.toString() shouldBe "kotlin.Unit"
-            fromResource(name.toInt()).test(fn::call)
-        }
-
-        fun testNearEquality(fn: KFunction<*>, tolerance: Double = 1e-9) {
-            val name = fn.name
-            println("Testing $name")
-            fn.returnType.toString() shouldBe "kotlin.Unit"
-            fromResource(name.toInt()).testNearEquality(fn::call, tolerance)
-        }
     }
 }
