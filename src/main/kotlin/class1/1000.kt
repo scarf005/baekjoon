929c8ebc5ec4d0731d9bca5b40a5b60255b15301
scarf <greenscarf005@gmail.com>
2023-01-05 15:51:09 +0900

    refactor: println at end

--- src/main/kotlin/class1/1000.kt
+++ src/main/kotlin/class1/1000.kt
@@ -1,4 +1,4 @@
 package class1
 
 /** [See](https://www.acmicpc.net/problem/1000) */
-fun `1000`() = println(readln().split(" ").map(String::toInt).reduce(Int::plus))
+fun `1000`() = readln().split("").map(String::toInt).reduce(Int::plus).let(::println)
